62c630fd0c024fe27c3572b931fdc54304e1f156
Curacerto <curacerto@gmail.com>
2025-02-26 13:28:21 +0000

    Weapon spawn operation

--- src/domain/weapon/weapon.module.ts
+++ src/domain/weapon/weapon.module.ts
@@ -1,6 +1,4 @@
 import {Module} from "@nestjs/common";
-import {PlayersController} from "../players/players.controller";
-import {PlayersService} from "../players/players.service";
 import {WeaponController} from "./weapon.controller";
 import {WeaponService} from "./weapon.service";
 import {CommandService} from "../command/command.service";
